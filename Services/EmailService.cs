870c42febca1b80b6aa98907393f95a11875e5e0
patriciosebastian <psebastiansalazar@gmail.com>
2021-10-14 21:58:53 -0700

    email service for contact form and added summernote css

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -19,9 +19,25 @@ namespace BlogProject.Services
             _mailsettings = mailsettings.Value;
         }
 
-        public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        public async Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailsettings.Mail);
+            email.To.Add(MailboxAddress.Parse(_mailsettings.Mail));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = $"<b>{name}</b> has sent you an email and can be reached at: <b>{emailFrom}</b><br/><br/>{htmlMessage}";
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailsettings.Host, _mailsettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailsettings.Mail, _mailsettings.Password);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
         }
 
         public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
